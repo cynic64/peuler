444b312c454347c90487fecd38d132c9b0ca6a1f
Nicholas Schweitzer <f3bl@gmx.com>
2020-12-06 13:40:40 +0100

    Small optimization

--- 736.cpp
+++ 736.cpp
@@ -9,11 +9,11 @@
 uint64_t paths_to_equality(uint64_t a, uint64_t b, int n);
 
 int main() {
-	for (auto n = 1; ; n += 1) {
+	for (auto n = 1; n < 20 ; n += 1) {
 		printf("%d\n", n);
 
-		auto x = paths_to_equality(45, 90, n);
-		if (x) printf("Final: %llu\n", x);
+		auto x = paths_to_equality(46, 180, n);
+		if (x) printf("Final: %lu\n", x);
 	}
 }
 
@@ -28,10 +28,18 @@ uint64_t paths_to_equality(uint64_t a, uint64_t b, int n) {
 	if (a == b && n != 1) return 0;
 	if (a == b && n == 1) return a;
 
-	auto rr = paths_to_equality(a+2, 4*b, n-1);
-	auto rs = paths_to_equality(2*a+1, 2*b+2, n-1);
-	auto sr = paths_to_equality(2*a+2, 2*b+1, n-1);
-	auto ss = paths_to_equality(4*a, b+2, n-1);
+	auto rr = 0, rs = 0, sr = 0, ss = 0;
+
+	// No point doing any r's if b > 2^(n+1)a
+	if (b < (2 << (n+1)) * a) {
+		rr = paths_to_equality(a+2, 4*b, n-1);
+		rs = paths_to_equality(2*a+1, 2*b+2, n-1);
+	}
+
+	if (a < (2 << (n+1)) * b) {
+		sr = paths_to_equality(2*a+2, 2*b+1, n-1);
+		ss = paths_to_equality(4*a, b+2, n-1);
+	}
 
 	return std::max({rr, rs, sr, ss});
 }
